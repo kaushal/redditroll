b8eedaaea5a1cd379126ec6cbb96afce6b2d6e73
Kaushal Parikh <krp103@kill.rutgers.edu>
2013-01-01 23:57:44 -0500

    wrote basic methods

--- troll.py
+++ troll.py
@@ -1,8 +1,16 @@
 from redditFunctions import *
+from trollFunctions import *
+from pprint import pprint as pp2
+
 
 def main():
     print 'the cake is a lie'
-    login('not_a_trollbot', 'trollbot')
-
+    wordList = {}
+    wordList["nouns"] = []
+    wordList["verbs"] = []
+    wordList = populateWordDict(wordList)
+    client = login('not_a_trollbot', 'trollbot')
+    j = subredditInfo(client, 1, 'funny', '', True)
+    pp2(j["data"])
 if __name__ == '__main__':
         main()
