2996450f11c881ffb27c48eb1bffd4b68e385df1
Kaushal Parikh <kaushalp88@kparikh.(none)>
2013-01-26 02:19:17 +0000

    made stuff work

--- prawTest/troll.py
+++ prawTest/troll.py
@@ -1,33 +1,35 @@
 import praw
-import time
+from time import sleep
 
 def populateWordDict(dict):
-	f = open("partsOfSpeech/nouns.txt", "r")
-	for line in f:
-		dict['nouns'].append(line)
-	f = open("partsOfSpeech/verbs.txt", "r")
-	for line in f:
-		dict["verbs"].append(line)
+    f = open("partsOfSpeech/nouns.txt", "r")
+    for line in f:
+        dict['nouns'].append(line)
+    f = open("partsOfSpeech/verbs.txt", "r")
+    for line in f:
+        dict["verbs"].append(line)
 
-	return dict
+    return dict
 
 def postReply(comments):
-	for comment in comments:
-		if comment.id not in already_done:
-			print comment.body
-			#comment.reply(comment.body)
-			already_done.append(comment.id)
+    already_done = []
+    for comment in comments:
+        if comment.id not in already_done:
+            comment.reply(comment.body)
+            already_done.append(comment.id)
 
 
 def main():
-	USER = "not_a_trollbot"
-	PASS = "trollbot"
-	r = praw.Reddit("Just for fun... :P")
-	r = praw.login(USER, PASS)
-	submission = r.get_submission(submission_id='11v36o')
-	flat_comments = submission.comments_flat
-	
-	subreddit = r.get_subreddit('all')
-	subreddit_comments = subreddit.get_comments()
-	postReply(subreddit_comments)
+    USER = "repetition_is_key"
+    PASS = "repetition"
+    r = praw.Reddit("Just for fun... :P")
+    r.login(USER, PASS)
+    submission = r.get_submission(submission_id='11v36o')
+    #flat_comments = submission.comments_flat
 
+    subreddit = r.get_subreddit('all')
+    subreddit_comments = subreddit.get_comments()
+    postReply(subreddit_comments)
+
+if __name__ == '__main__':
+    main()
