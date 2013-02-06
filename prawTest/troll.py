133641f700d82e83dd64974c203ab66124d80aae
Kaushal Parikh <kaushalp88@kparikh.(none)>
2013-02-06 01:09:11 +0000

    updated some shit basic functionality works

--- prawTest/troll.py
+++ prawTest/troll.py
@@ -14,22 +14,23 @@ def populateWordDict(dict):
 def postReply(comments):
     already_done = []
     for comment in comments:
-        if comment.id not in already_done:
-            comment.reply(comment.body)
-            already_done.append(comment.id)
-
+        if "ironic" in str(comment.body):
+            comment.reply("Actually that isn't even ironic. It's just coincidental.")
+        elif "idea" in str(comment.body):
+            comment.reply("Wow. That's the stupidest idea I have ever heard!")
+        print comment.body
+        print "\n"
 
 def main():
     USER = "repetition_is_key"
     PASS = "repetition"
     r = praw.Reddit("Just for fun... :P")
     r.login(USER, PASS)
-    submission = r.get_submission(submission_id='11v36o')
-    #flat_comments = submission.comments_flat
-
-    subreddit = r.get_subreddit('all')
-    subreddit_comments = subreddit.get_comments()
-    postReply(subreddit_comments)
+    while True:
+        subreddit = r.get_subreddit('all')
+        subreddit_comments = subreddit.get_comments()
+        postReply(subreddit_comments)
+        sleep(300)
 
 if __name__ == '__main__':
     main()
