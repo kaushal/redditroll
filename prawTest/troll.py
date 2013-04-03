bfb4de7cf2b15d5130744f5fb1c76f201b5e810d
Sam Agnew <sagnew@eden.rutgers.edu>
2013-04-03 00:05:26 -0400

    Put everything in a loop, and logs into a new account each time

--- prawTest/troll.py
+++ prawTest/troll.py
@@ -2,6 +2,13 @@ import praw
 from time import sleep
 import condescending
 
+#List of accounts of our bots
+__accounts = ["repetition_is_key"]
+
+#Dictionary associating accounts with their respective passwords
+__passwords = {"repetition_is_key": repetition
+        }
+
 def populateWordDict(dict):
     f = open("partsOfSpeech/nouns.txt", "r")
     for line in f:
@@ -21,16 +28,22 @@ def postReply(comments):
         print comment.body
         print "\n"
 
+def switch_accounts(current_acc):
+    new_acc = random.choice(__accounts)
+    while not new_acc == current_acc:
+        return new_acc, __passwords[new_acc]
+
 def main():
-    USER = "repetition_is_key"
-    PASS = "repetition"
-    r = praw.Reddit("Just for fun... :P")
-    r.login(USER, PASS)
+    USER = ""
     while True:
-        subreddit = r.get_subreddit('all')
-        subreddit_comments = subreddit.get_comments()
-        postReply(subreddit_comments)
-        sleep(300)
+        r = praw.Reddit("Just for fun... :P")
+        USER, PASS = switch_accounts(USER)
+        r.login(USER, PASS)
+        while True:
+            subreddit = r.get_subreddit('all')
+            subreddit_comments = subreddit.get_comments()
+            postReply(subreddit_comments)
+            sleep(300)
 
 if __name__ == '__main__':
     main()
